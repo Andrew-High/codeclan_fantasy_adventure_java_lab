0302876e2661bc49660521cfbe34be7f106c24a8
MistaRae <itsamistarae@gmail.com>
2021-06-16 11:32:13 +0100

    Changes by user

---
+++ src/main/java/Character.java
@@ -0,0 +1,25 @@
+public abstract class Character {
+    private int hitPoints;
+    private int stamina;
+
+    public Character(int hitPoints, int stamina) {
+        this.hitPoints = hitPoints;
+        this.stamina = stamina;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+}
