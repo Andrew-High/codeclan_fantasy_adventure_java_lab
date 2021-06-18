95813fab6f3991ec9c2d696210cf1e3d61f36704
Andrew High <andrewhigh4@hotmail.com>
2021-06-18 14:55:39 +0100

    added rooms to dungeon

--- src/main/java/dungeon/Dungeon.java
+++ src/main/java/dungeon/Dungeon.java
@@ -1,4 +1,12 @@
 package dungeon;
 
+import java.util.ArrayList;
+
 public class Dungeon {
+
+    private ArrayList<Room> rooms;
+
+    public Dungeon () {
+        this.rooms = new ArrayList<>();
+    }
 }
